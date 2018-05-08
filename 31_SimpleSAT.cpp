bf334d58750c99401051aaf38fe04dbb15dce993
Carlos Leonard <carlos.leonard@binarysword.com>
2018-05-08 16:06:44 -0400

    SimpleSAT completed

--- 31_SimpleSAT.cpp
+++ 31_SimpleSAT.cpp
@@ -7,14 +7,131 @@ A Boolean formula will always have at most 5 letters, i.e. a, b, c, d and e. A t
 
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
-// NOT FINISHED
+bool checkFormula(map <char, bool>, string, int&);
 
+/*
+traverse the string input
+any valid letters will be stored inside a map
+by default their bool value will be set to true
+only condition in which they are set to false is if there is an AND NOT sequence
+
+to analyze the formula we will use a recursive method
+we keep track of our overall bool value for the section we are analyzing
+at each iteration, by storing our operator we update our bool value based on the current value
+for example if operator is & and previous overall value is true but current is false
+our overall value is updated to false
+we make a recursive call for each opening parenthesis
+at the same time we update our index
+*/
 string SimpleSAT(string str) 
 {
+	map <char, bool> table;
 
+	// traversing the string input to extract our letters
+	for (int x = 0; x < str.length(); x++)
+	{
+		// getting the letters
+		if (str[x] >= 'a' && str[x] <= 'z')
+		{
+			table[str[x]] = true;
+		}
 
+		// checking for special condition of an AND NOT sequence
+		if (str[x] == '&' && str[x + 1] == '~')
+		{
+			x += 2;
+			table[str[x]] = false;
+		}
+	}
+
+	int start = 0;
+	if (checkFormula(table,str, start)) // calling our method to check if the boolean formula is true or false
+	{
+		return "yes";
+	}
+	else
+	{
+		return "no";
+	}
+}
+
+// method to analyze the boolean formula from the string input
+bool checkFormula(map <char, bool> table, string str, int& index)
+{
+	int currentResult = 0;
+	char currentOperator = 'x';
+	bool notEqual = false;
+
+	// traversing the string to analyze it's content
+	for (; index < str.length(); index++)
+	{
+		// condition to enter and analyze a parenthesis section
+		if (str[index] == '(')
+		{
+			index++;
+			currentResult = checkFormula(table, str, index); // getting the result from that section
+		}
+		else if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			// condition to analyze current value based on our rules
+			// get current boolean value and determine if a not operator was applied to it
+			bool temp;
+			if (notEqual)
+			{
+				temp = !table[str[index]];
+			}
+			else
+			{
+				temp = table[str[index]];
+			}
+			notEqual = false;
+
+			// analyze which operator was included
+			if (currentOperator == '&')
+			{
+				// we update our current result only if previous was true
+				if (currentResult)
+				{
+					currentResult = temp;
+				}
+			}
+			else if (currentOperator == '|')
+			{
+				// we can replace our current result only if new boolean value is true
+				if (temp)
+				{
+					currentResult = temp;
+				}
+			}
+			else
+			{
+				currentResult = temp;
+			}
+		}
+		else if (str[index] == '&') // collect the AND operator
+		{
+			currentOperator = '&';
+		}
+		else if (str[index] == '|') // collect the OR operator
+		{
+			currentOperator = '|';
+		}
+		else if (str[index] == '~') // collect the NOT operator
+		{
+			notEqual = true;
+		}
+		else if (str[index] == ')')
+		{
+			// sending back our result pertaining to a section
+			// this represents our base case
+			return currentResult;
+		}
+	}
+
+	return currentResult;
 }
 
 int main() 
@@ -23,5 +140,7 @@ int main()
 	cout << SimpleSAT("((a&c)&~a)") << endl; // no
 	cout << SimpleSAT("(a&b&c)|~a") << endl; // yes
 	cout << SimpleSAT("a&(b|c)&~b&~c") << endl; // no
+	cout << SimpleSAT("(a&(b)&~c)") << endl; // yes
+	cout << SimpleSAT("(c|a|b)&~c") << endl; // yes
 	return 0;
 }
